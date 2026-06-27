5aa6b1076e77d3bcc0bb5760ae97dafeca0b21a5
Mahyar993 <163139422+Mahyar993@users.noreply.github.com>
2026-06-27 16:02:52 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_BASE_URL = "https://khaberni-backend-production.up.railway.app";
+const API_BASE_URL = "https://khaberni-backend.onrender.com";
 
 const DEFAULT_SECTIONS = [
   { id: "Water", title: "مياه اليوم" },
